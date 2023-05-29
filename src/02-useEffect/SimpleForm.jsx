efadf4d210fd2b1ec5035da8c02f405ed1c5588d
Jessica Villa <suhkha92@gmail.com>
2023-05-29 12:24:26 -0600

    useEffect dependencies

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -17,8 +17,16 @@ export const SimpleForm = () => {
     }
 
     useEffect(() => {
-        console.log('useEffect');
-    });
+        console.log('useEffect called');
+    }, []);
+
+    useEffect(() => {
+        console.log('formState changed');
+    }, [formState]);
+
+    useEffect(() => {
+        console.log('email changed');
+    }, [email]);
 
     return (
         <>
